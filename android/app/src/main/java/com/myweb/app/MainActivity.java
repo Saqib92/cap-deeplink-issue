429627514b17196e0faff318d048acd05d6f7219
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2024-01-24 20:13:06 +0500

    project setup with issue

---
+++ android/app/src/main/java/com/myweb/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myweb.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
+import { Platform } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +12,29 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private router: Router,
+    private zone: NgZone
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.setupDeepLink();
+    })
+  }
+
+  setupDeepLink() {
+    App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+      console.log('deeplink event: ', event)
+      this.zone.run(() => {
+        // handel deeplink response
+      });
+    });
+  }
+
+
+
 }
